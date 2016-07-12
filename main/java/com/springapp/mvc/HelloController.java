7a03ec97fe89a2384314d8d750565acaedf1795d
DeWinters <derekwwinters@gmail.com>
2016-07-12 08:54:45 +0100

    Add files via upload
    Items and Sales db additions

--- main/java/com/springapp/mvc/HelloController.java
+++ main/java/com/springapp/mvc/HelloController.java
@@ -19,24 +19,31 @@ public class HelloController {
 
     String owner = "TheStudentProgrammer";
     String confirm ="";
+    long hack = 0;                                                                  // this will be made clear
 
 	@RequestMapping(value="/",method = RequestMethod.GET)                           /** Working as intended */
 	public String printWelcome(ModelMap model) {
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSauce);
+        //JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSauce);
 		model.addAttribute("greeting", "Enter New Member details");
         confirm = "New Member Entry";
 		return "index";
 	}
 
-
-    @RequestMapping(value="/editor",method = RequestMethod.POST)                    /** Needs default values from selected member's db data */
+    /***************************************** members *********************************************************/
+    @RequestMapping(value="/editor",method = RequestMethod.POST)                    /** this looping construct can't be right. a single user obj would be better than a List obj */
     public String editMember(ModelMap model,@RequestParam("editId") long id){
         JdbcTemplate jdbctemplate = new JdbcTemplate(dataSauce);
-        //User subject = jdbctemplate.query("SELECT username FROM USERS WHERE id =5", new UserMapper());   // I cant get this restricted query to work
-        List<User> allUsers = jdbctemplate.query("SELECT * FROM USERS", new UserMapper());
+        //String xxx="";
+        //jdbctemplate.query("SELECT username FROM USERS WHERE id =5", xxx);                // nope
+        //xxx = jdbctemplate.query("SELECT username FROM USERS WHERE id=5");                // nope
+        //User a = jdbctemplate.query("SELECT * FROM USERS WHERE ID =?", id);               // nope
+        //User b = jdbctemplate.query("SELECT * FROM USERS WHERE ID =5", new UserMapper()); // nope
+
+        List<User> allUsers = jdbctemplate.query("SELECT * FROM USERS WHERE ID =?", new UserMapper(), id);
         String username ="";
         String password ="";
         int age = 0;
+
         for (User member : allUsers){
             if (member.getId() == id) {
                 username = member.getUsername();
@@ -48,8 +55,9 @@ public class HelloController {
         model.addAttribute("pass", password);
         model.addAttribute("age", age);
         model.addAttribute("id", id);
+        hack = id;
         confirm = "Member Data Edit";
-        return "editor";
+        return "members/editor";
     }
 
 
@@ -59,11 +67,11 @@ public class HelloController {
         jdbcTemplate.update("INSERT INTO USERS(username,password,age) VALUES(?,?,?)",username,password,age);
         model.addAttribute("username", username);
         model.addAttribute("procedure", confirm);
-        return "confirmation";
+        return "members/confirmation";
     }
 
 
-    @RequestMapping(value="/census", method = RequestMethod.POST)                   /** Working as intended */
+    @RequestMapping(value="/memberList", method = RequestMethod.POST)               /** Working as intended */
     public String listMembers(ModelMap model) {
         /** Java solution */
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSauce);
@@ -105,27 +113,107 @@ public class HelloController {
         }
 
 
-
         System.out.println(allUsers.iterator());                // failed:seems to find address of table instead of contents of table elements
         model.addAttribute("userCount", allUsers.size());       // works: posts the total number of table entries
         model.addAttribute("owner", owner);                     // works: posts a string to the page
         model.addAttribute("roleCall", theLot);                 // works: Java solution to age data restriction
         model.addAttribute("theDraft", justAdults);             // works: Query solution to age data restriction
         model.addAttribute("eligible", troops);                 // works: counts total adults
-        return "memberList";
+        return "members/memberList";
     }
 
 
-    @RequestMapping(value="/changed", method = RequestMethod.POST)
-    public String editMember(ModelMap model,@RequestParam("newName") String username, @RequestParam("newPass") String password, @RequestParam("newAge") String age, long id){
+    @RequestMapping(value="/changed", method = RequestMethod.POST)                  /** Working... */
+    public String editMember(ModelMap model, @RequestParam("newName") String username, @RequestParam("newPass") String password, @RequestParam("newAge") String age){
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSauce);
-        jdbcTemplate.update("UPDATE USERS SET(username,password,age) VALUES(?,?,?) WHERE id =(?) ", username, password, age, id);
-
-
-      return "changed";
+        jdbcTemplate.update("UPDATE USERS SET username =?, password =?, age =? WHERE id =?", username, password, age, hack);
+        model.addAttribute("procedure", confirm);
+        return "members/changed";
     }
 
 
+    @RequestMapping(value="/delete", method = RequestMethod.POST)                   /** Working... */
+    public String deleteMember(ModelMap model) {
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSauce);
+        jdbcTemplate.update("DELETE FROM USERS WHERE ID =?", hack);
+        model.addAttribute("procedure", "Delete Member Record");
+        return "members/changed";
+    }
+
+    /***************************************** items ************************************************************/
+    @RequestMapping(value="/shelves", method = RequestMethod.POST)                  /** Working as intended */
+    public String shelf(ModelMap model) {
+        String allStock ="";
+        JdbcTemplate jdbcT = new JdbcTemplate(dataSauce);
+        List<Item> itemList = jdbcT.query("SELECT * FROM STOCK", new ItemMapper());
+        for(Item item : itemList){
+            allStock += item.getItemId() +" ";
+            allStock += item.getItemName() +" ";
+            allStock += item.getItemPrice() +"<br>";
+        }
+        model.addAttribute("stockList", allStock);
+        return "items/shelves";
+    }
+
+
+    @RequestMapping(value="/addItem", method = RequestMethod.POST)                  /** Working as intended */
+    public String newItem(ModelMap model, @RequestParam("itemId") String id, @RequestParam("itemName") String name, @RequestParam("itemPrice") float price){
+        JdbcTemplate jdbcTemp = new JdbcTemplate(dataSauce);
+        jdbcTemp.update("INSERT INTO STOCK(itemId,itemName,itemPrice) VALUES(?,?,?)", id, name, price);
+        model.addAttribute("id", id);
+        model.addAttribute("name", name);
+        model.addAttribute("price", price);
+        return "items/addItem";
+    }
+
+
+    @RequestMapping(value="/sellItem", method = RequestMethod.POST)                 /** Working: add more user feedback */
+    public String sellItem(ModelMap model, @RequestParam("itemId") String itemId, @RequestParam("memberId") long memberId){
+        JdbcTemplate jdbcT = new JdbcTemplate(dataSauce);
+        jdbcT.update("INSERT INTO SALES(memberId,itemId,price,date) VALUES(?,?,?,?)", memberId, itemId, 19.95, "fakeDate");
+        model.addAttribute("memberId", memberId);
+        model.addAttribute("itemId", itemId);
+
+        jdbcT.update("DELETE FROM STOCK WHERE itemId =?", itemId);
+        return "items/sellItem";
+    }
+
+
+    @RequestMapping(value="/salesLog", method = RequestMethod.POST)                 /* Build sale class and saleMapper */
+    public String allSales(ModelMap model){
+        JdbcTemplate jdbcT = new JdbcTemplate(dataSauce);
+        List<Sale> allSales = jdbcT.query("SELECT * FROM SALES", new SaleMapper());
+        String history ="";
+        for(Sale sale : allSales){
+            history += sale.getInvoice() +" "+
+                       sale.getDate() +" "+
+                       sale.getMemberId() +" "+
+                       sale.getItemId() +" "+
+                       sale.getPrice() +"<br>";
+        }
+        model.addAttribute("history", history);
+        return "salesLog";
+    }
+
+
+    @RequestMapping(value="/findItem", method = RequestMethod.POST)                 /** process item search: partially working: add search conditions */
+    public String searchItem(ModelMap model, @RequestParam("findId") String id, @RequestParam("findName") String name, @RequestParam("maxPrice") float max){
+        JdbcTemplate jdbcT = new JdbcTemplate(dataSauce);
+        List<Item> result = jdbcT.query("SELECT * FROM STOCK WHERE itemName =?", new ItemMapper(), name);
+        String nameMatches ="";
+
+        /** Make a conditional statement to determine which cell to ue as search criteria **/
+        for(Item match : result){
+            nameMatches += match.getItemId() +" "+
+                           match.getItemName() +" "+
+                           match.getItemPrice() +"<br>";
+        }
+        model.addAttribute("results", nameMatches);
+        return "items/findItem";
+    }
+
+
+    /***************************************** math *************************************************************/
     @RequestMapping(value="/addIt", method = RequestMethod.POST)                    /** Working as intended */
     public String addStuff(ModelMap model,@RequestParam("cell1") float cell1, @RequestParam("cell2") float cell2){
         float answer = cell1 + cell2;
---
+++ main/webapp/WEB-INF/pages/salesLog.jsp
@@ -0,0 +1,9 @@
+<html>
+    <head>
+    </head>
+
+    <body>
+    <h3>Full Sales Report</h3>
+    ${history}
+    </body>
+</html>
