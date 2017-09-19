1a503209de166a6f00ebeeb81126664471774851
DeWinters <derekwwinters@gmail.com>
2017-09-19 22:55:55 +0100

    prepared statements

--- src/main/java/com/springapp/mvc/DAO.java
+++ src/main/java/com/springapp/mvc/DAO.java
@@ -5,15 +5,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
-import org.springframework.stereotype.Service;
-import org.springframework.ui.ModelMap;
-
-import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
-/**
- * Created by Administrator on 07/09/2017.
- */
+
 @Service
 public class DAO {
 
@@ -43,14 +37,12 @@ public class DAO {
 
     public void updateGuest(long id, String firstName, String lastName){
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSauce);
-        jdbcTemplate.update("UPDATE guests SET first_name ='" + firstName +
-                                                    "', last_name ='" + lastName +
-                                                    "' WHERE guest_id =" + id);
+        jdbcTemplate.update("UPDATE guests SET first_name = ? , last_name = ? WHERE guest_id =?", firstName, lastName, id);
     }
 
     public void deleteGuest(long id){
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSauce);
-        jdbcTemplate.update("DELETE FROM guests WHERE guest_id =" + id);
+        jdbcTemplate.update("DELETE FROM guests WHERE guest_id =?", id);
     }
 
     public String getGuestLog(){
